648f28898e794b1f87dd90564b45e172272be4ec
Thirza <thirzahmad@gmail.com>
2024-01-08 23:19:28 +0700

    feat: add more backend

--- newsapi/models.py
+++ newsapi/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class NewNews(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    url_image = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
